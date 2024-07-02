bfdedab048884c8c80b5f40628d944fba236e9cf
IEAD\1910003 <russel.chua@iisigroup.com>
2024-07-02 12:57:50 +0800

    [Home] WEB樣式基本完成.

--- shi/apps/shi/src/app/Feature/FooterFrame/FooterFrame.vue
+++ shi/apps/shi/src/app/Feature/FooterFrame/FooterFrame.vue
@@ -8,28 +8,35 @@
     <!-- 內容內部排版 置中 -->
     <div class="max-w-[95rem] my-0 mx-auto">
 
-      <!-- 資訊 -->
-      <div class="flex items-center pt-9 gap-2">
-        <p class="p-3 font-roboto text-lg font-normal leading-[1.625rem] tracking-[0.0625rem] text-[#FFFFFF]">
-          教練電話：0988786796
-        </p>
+      <!-- 外框 -->
+       <div class="flex justify-between">
+         <!-- 左邊 -->
+          <div>
+            <!-- 資訊 -->
+            <div class="flex items-center pt-9 gap-2">
+              <p class="p-3 font-roboto text-lg font-normal leading-[1.625rem] tracking-[0.0625rem] text-[#FFFFFF]">
+                教練電話：0988786796
+              </p>
 
-        <!-- 分隔線 -->
-        <div class="w-[0.125rem] h-6 border-[#FFFFFF] border-[0.08125rem]"></div>
+              <!-- 分隔線 -->
+              <div class="w-[0.125rem] h-6 border-[#FFFFFF] border-[0.08125rem]"></div>
 
-        <p class="p-3 font-roboto text-lg font-normal leading-[1.625rem] tracking-[0.0625rem] text-left text-[#FFFFFF]">
-          王羿文教練
-        </p>
-      </div>
+              <p class="p-3 font-roboto text-lg font-normal leading-[1.625rem] tracking-[0.0625rem] text-left text-[#FFFFFF]">
+                王羿文教練
+              </p>
+            </div>
 
-      <!-- 版權 -->
-      <div class="flex justify-between">
-        <span class="px-3 py-2 items-start min-h-8 font-['Noto_Sans_TC'] text-base font-normal leading-8 tracking-[0.125rem] text-left text-[#FFFFFF]">
-          COPYRIGHT 2024 Shi HSINCHU SHI INLINE SKATE, ALL RIGHTS RESERVED. 恃 Shi Holdings
-        </span>
+            <!-- 版權 -->
+            <div class="flex justify-between">
+              <span class="px-3 py-2 items-start min-h-8 font-['Noto_Sans_TC'] text-base font-normal leading-8 tracking-[0.125rem] text-left text-[#FFFFFF]">
+                COPYRIGHT 2024 Shi HSINCHU SHI INLINE SKATE, ALL RIGHTS RESERVED. 恃 Shi Holdings
+              </span>
+            </div>
+          </div>
 
-        <img class="mt-2" src="../../../assets/images/a11y_logo.png" alt="恃" />
-      </div>
+         <!-- 右邊 -->
+         <img class="" src="../../../assets/images/Footer/shi_4x.png" alt="恃" />
+       </div>
 
     </div>
   </footer>
--- shi/apps/shi/src/app/Feature/Home/Home.vue
+++ shi/apps/shi/src/app/Feature/Home/Home.vue
@@ -6,24 +6,28 @@
   <!-- 外圈 -->
   <div class="flex flex-col gap-14">
     <!-- 區塊1 -->
-    <div class="flex justify-between items-center gap-1">
+    <div class="flex justify-center items-center gap-1">
       <!-- 左邊 -->
-       <img class="min-w-[207px] min-h-[131px]" src="../../../assets/images/Home/picture_1.png" alt="picture_1">
+       <img class="min-w-[207px] min-h-[131px]" src="../../../assets/images/Home/picture_1_4x.png" alt="picture_1">
       <!-- 右邊 -->
-       <div class="min-w-[163px] min-h-[145px] bg-[#C7B6CB] rounded-full"></div>
+      <div class="flex justify-center items-center w-[40%] sm:w-[50%] md:w-[60%] lg:w-[70%] h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] min-w-[163px] min-h-[145px] max-w-[702px] max-h-[527px] bg-[#C7B6CB] rounded-full">
+        <p class="w-full break-words text-center px-4 text-[1rem] sm:text-base md:text-xl lg:text-2xl xl:text-3xl">夏令營</p>
+      </div>
     </div>
 
     <!-- 區塊2 -->
-    <div class="flex justify-between items-center">
+    <div class="flex justify-center items-center">
       <!-- 左邊 -->
-      <div class="min-w-[91px] min-h-[89px]"></div>
+      <div class="flex justify-center items-center w-[40%] sm:w-[50%] md:w-[60%] lg:w-[70%] h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] min-w-[163px] min-h-[145px] max-w-[702px] max-h-[527px] rounded-lg">
+        <p class="w-full break-words text-center p-2 text-[1rem] sm:text-base md:text-xl lg:text-2xl xl:text-3xl">竹北第一</p>
+      </div>
       <!-- 右邊 -->
-       <img class="min-w-[215px] min-h-[159px]" src="../../../assets/images/Home/picture_2.png" alt="picture_2">
+       <img class="min-w-[215px] min-h-[159px]" src="../../../assets/images/Home/picture_2_4x.png" alt="picture_2">
     </div>
 
     <!-- 區塊3 -->
-    <div class="flex justify-center items-center min-w-[349px] min-h-[157px] bg-[#111111] rounded-[22px] mb-12">
-      <p class="text-[#FFFFFF]">招生影片</p>
+    <div class="flex justify-center items-center w-full min-h-[38rem] bg-[#111111] rounded-[22px] mb-12">
+      <p class="text-[#FFFFFF] text-[3rem]">招生影片</p>
     </div>
   </div>
 
